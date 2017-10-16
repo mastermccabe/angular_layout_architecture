9ede51f5a47d1ab1fc9b38f425945846e12b9125
McCabe <mccabe@wambl.com>
2017-10-16 13:38:25 -0700

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { HeaderComponentComponent } from './task/header-component/header-component.component';
+import { NavComponentComponent } from './task/nav-component/nav-component.component';
+import { MainComponentComponent } from './task/main-component/main-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    HeaderComponentComponent,
+    NavComponentComponent,
+    MainComponentComponent
   ],
   imports: [
     BrowserModule
